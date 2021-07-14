2518f6facbce8163866179fdc00b0118065c7083
testuser681 <piotrdrabicki99@gmail.com>
2021-07-14 15:59:29 +0200

    containers working fine

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+docker run -dit -p 2222:22 --name alpine --network="my-overlay" --rm -v /sys/fs/cgroup/:/sys/fs/cgroup:ro --cap-add SYS_ADMIN --cap-add NET_ADMIN alpine
 docker run -dit -p 2223:22 --name ubuntu --network="my-overlay" --rm -v /sys/fs/cgroup/:/sys/fs/cgroup:ro --cap-add SYS_ADMIN --cap-add NET_ADMIN ubuntu
 docker run -dit -p 2224:22 --name fedora --network="my-overlay" --rm -v /sys/fs/cgroup/:/sys/fs/cgroup:ro --cap-add SYS_ADMIN --cap-add NET_ADMIN fedora
 
