53a544eadba4d8fc3bae36bc735ca35b101ce59d
testuser681 <piotrdrabicki99@gmail.com>
2021-07-15 11:35:10 +0200

    role

--- ansible/start.sh
+++ ansible/start.sh
@@ -7,9 +7,9 @@ socat UDP-LISTEN:7946,reuseaddr,fork UNIX-CONNECT:/var/run/docker.sock &
 socat UDP-LISTEN:4789,reuseaddr,fork UNIX-CONNECT:/var/run/docker.sock &
 
 # build docker images
-docker build -t alpine ./alpine
-docker build -t fedora ./fedora
-docker build -t ubuntu ./ubuntu
+#docker build -t alpine ./alpine
+#docker build -t fedora ./fedora
+#docker build -t ubuntu ./ubuntu
 
 # create overlay network
 docker network create -d overlay --attachable my-overlay
